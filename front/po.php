26ffd9f97838d4040ab7229b35245c87d662f62a
Li-Ting-Huang <help>
2022-08-03 22:37:36 +0800

    建立分類網誌基本外觀及點擊功能

--- front/po.php
+++ front/po.php
@@ -1,1 +1,21 @@
-分類網誌
+<div>目前位置 : 首頁 > 分類網誌 > <span id="header"></span></div>
+<div style="display: flex;">
+<fieldset>
+    <legend>分類網誌</legend>
+    <div class="type">健康新知</div>
+    <div class="type">菸害防制</div>
+    <div class="type">癌症防治</div>
+    <div class="type">慢性病防治</div>
+</fieldset>
+<fieldset>
+    <legend>文章列表</legend>
+    <div></div>
+</fieldset>
+</div>
+
+<script>
+    $(".type").on("click",function(){
+        let type=$(this).text()
+        $("#header").text(type)
+    })
+</script>
