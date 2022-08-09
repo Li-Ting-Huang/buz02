9f94a8dfbc56fbb3f6be0a07eb3eaaae3e95ce62
Li-Ting-Huang <help>
2022-08-09 19:24:30 +0800

    完成最新文章點擊顯示完整內容功能

--- base.php
+++ base.php
@@ -4,116 +4,114 @@ date_default_timezone_set("Asia/Taipei");
 
 class DB{
     protected $table;
-    protected $dsn="mysql:host=localhost;charset=utf8;dbname=web02;";
+    protected $dsn="mysql:host=localhost;charset=utf8;dbname=web02";
     protected $pdo;
-
 
     function __construct($table)
     {
         $this->table=$table;
         $this->pdo=new PDO($this->dsn,'root','');
+
     }
-// all
+
     function all(...$arg){
         $sql="select * from $this->table ";
-        
         if(isset($arg[0])){
-             if(is_array($arg[0])){
+            if(is_array($arg[0])){
                 foreach($arg[0] as $key => $val){
-                    $tmp[]="`$key`='$val'";
+                    $tmp[]="`$key` = '$val'";
                 }
-                // $sql=$sql." where " . join(" && ,$tmp");
-                $sql.=" where " . join(" && ",$tmp);
-             }else{
-                // $sql=$sql . $arg[0];
-                $sql.= $arg[0];
-             }
+                $sql.=" where ".join(" && ",$tmp);
+            }else{
+
+                $sql.=$arg[0];
+            }
+
         }
         if(isset($arg[1])){
-            $sql .= $arg[1];
+            $sql.=$arg[1];
         }
-        // echo $sql;
         return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
-//find
+
+    //find
+
     function find($arg){
-        //單筆
         $sql="select * from $this->table ";
-        // 判斷是否為陣列
-        if(is_array($arg)){
-            //陣列
-            foreach($arg as $key => $val){
-                $tmp[]="`$key`='$val'";
-            }
-            // 陣列
-            $sql.= " where ". join(" && ",$tmp);
-        }else{
-            // id
-            $sql .= " where `id`= '$arg'";
         
-        }
-        // echo $sql;
-        //單筆
+            if(is_array($arg)){
+                foreach($arg as $key => $val){
+                    $tmp[]="`$key` = '$val'";
+                }
+                $sql.=" where ".join(" && ",$tmp);
+            }else{
+
+                $sql.=" where `id` ='$arg'";
+            }
+
+       
+        
         return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+    }
 
-    }
-// save
-    function save($array){
-        if(isset($array['id'])){//有ID更新
-            
-            foreach($array as $key => $val){
-                $tmp[]="`$key`='$val'";
-            }
-            // updwte $this->table set `date`='2022-07-28',`name`='XXx'
-            $sql="update $this->table set ".join(',',$tmp)."  where `id`='{$array['id']}'";
-        }else{//沒有ID新增
-            $sql="insert into $this->table (`".join("`,`",array_keys($array))."`) 
-            values ('".join("','",$array)."')";
-        }
-        return $this->pdo->exec($sql);
-    }
-// del
+    //del
     function del($arg){
-        $sql="delete from $this->table where ";
-        if(is_array($arg)){
-            foreach($arg as $key => $val){
-                $tmp[]="`$key`='$val'";
-            }
-            $sql .= join(" && " ,$tmp);
-        }else{
-            $sql .= " `id` = '$arg'";
-        }
-        echo $sql;
-        return $this->pdo->exec($sql);
+        $sql="delete from $this->table ";
+        
+            if(is_array($arg)){
+                foreach($arg as $key => $val){
+                    $tmp[]="`$key` = '$val'";
+                }
+                $sql.=" where ".join(" && ",$tmp);
+            }else{
 
+                $sql.=" where `id` ='$arg'";
+            }
+
+       
+        
+        return $this->pdo->exec($sql);
     }
-//math 
+
+    function save($array){
+        
+        // 更新
+            if(isset($array['id'])){
+                foreach($array as $key => $val){
+                    $tmp[]="`$key` = '$val'";
+                }
+                // $sql.=" where ".join(" && ",$tmp);
+                $sql="update $this->table set ".join(',',$tmp)." where `id`= '{$array['id']}'";
+            }else{
+        // 新增
+                
+                // $sql.=" where `id` ='$arg'";
+                $sql="insert into $this->table (`".join("`,`",array_keys($array))."`) values ('".join("','",$array)."')";
+            }
+
+       
+        
+        return $this->pdo->exec($sql);
+    }
+    
     function math($math,$col,...$arg){
         $sql="select $math($col) from $this->table ";
-        
         if(isset($arg[0])){
-             if(is_array($arg[0])){
+            if(is_array($arg[0])){
                 foreach($arg[0] as $key => $val){
-                    $tmp[]="`$key`='$val'";
+                    $tmp[]="`$key` = '$val'";
                 }
-                // $sql=$sql." where " . join(" && ,$tmp");
-                $sql.=" where " . join(" && ",$tmp);
-             }else{
-                // $sql=$sql . $arg[0];
-                $sql.= $arg[0];
-             }
+                $sql.=" where ".join(" && ",$tmp);
+            }else{
+
+                $sql.=$arg[0];
+            }
+
         }
         if(isset($arg[1])){
-            $sql .= $arg[1];
+            $sql.=$arg[1];
         }
-        // echo $sql;
         return $this->pdo->query($sql)->fetchColumn();
-
-    }
-
-    
-    function q($sql){
-        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
 }
 function dd($array){
@@ -130,23 +128,4 @@ $Total=new DB('total');
 $User=new DB('user');
 $News=new DB('news');
 $Que=new DB('que');
-
-// print_r($Total->find(['date'=>'2022-08-02']));
-
-//如果沒有SESSION以下不執行
-if(!isset($_SESSION['total'])){
-    // 找到當天日期
-    $total=$Total->find(['date'=>date("Y-m-d")]);  
-    // 連覽+1
-    $total['total']=$total['total']+1;
-    // 儲存資料庫
-    $Total->save($total);
-    
-    // 增加SESSION，所以有SESION此程式不會再重新回到上面重跑
-    $_SESSION['total']=$total['total'];
-
-  
-    }
-   
-        
 ?>
---
+++ front/pop.php
@@ -0,0 +1,71 @@
+<fieldset>
+    <legend>目前位置 : 首頁 > 人氣文章區</legend>
+    <table id="news">
+        <tr>
+            <td width="30%">標題</td>
+            <td width="50%">內容</td>
+            <td width="20%">人氣</td>
+        </tr>
+        <?php
+        $all = $News->math('count', 'id', ['sh' => 1]);
+        $div = 5;
+        $pages = ceil($all / $div);
+        $now = $_GET['p'] ?? 1;
+        $start = ($now - 1) * $div;
+
+        // $rows = $News->all(['sh' => 1], " limit $start,$div");
+        // 加 order by good desc "
+        $rows = $News->all(['sh' => 1], " order by good desc limit $start,$div");
+
+        foreach ($rows as $row) {
+        ?>
+            <tr>
+                <td class="title clo" style="cursor: pointer;"><?= $row['title']; ?></td>
+                <!-- 加td class="pop" -->
+                <td class="pop">
+                    <span class="summary"><?= mb_substr($row['text'], 0, 20); ?>...</span>
+                    <!-- 改 -->
+                    <div class="modal">
+                    <!-- <span class="full" style="display:none ;"> -->
+                        <?=nl2br($row['text']);?>
+                    </span>
+                    </div>
+                </td>
+                <!-- mb_substr取的部分字串 -->
+                <td></td>
+            </tr>
+        <?php
+        }
+        ?>
+    </table>
+
+    <div>
+        <?php
+        if (($now - 1) > 0) {
+            $p = $now - 1;
+            echo "<a href='?do=news&p={$p}'> < </a>";
+        }
+
+        for ($i = 1; $i <= $pages; $i++) {
+            $fontsize = ($now == $i) ? '24px' : '18px';
+            echo "<a href='?do=news&p={$i}' style='font-size:{$fontsize}'> $i </a>";
+        }
+        if (($now + 1) <= $pages) { //<=等於也要寫
+            $p = $now + 1;
+            echo "<a href='?do=news&p={$p}'> > </a>";
+        }
+        ?>
+    </div>
+
+</fieldset>
+<script>
+    // 加.pop
+    $('.title,.pop').hover(
+        function(){
+            $(this).parent().find('.modal').show()
+        },
+        function(){
+            $(this).parent().find('.modal').hide()
+        }
+    )
+</script>
