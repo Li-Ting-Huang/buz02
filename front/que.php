ebcbca61907a15fc735a0d1a95e17e6e7d4603ed
k2453540 <k2453540@gmail.com>
2022-08-10 13:21:38 +0800

    建立前台顯示問卷

--- front/que.php
+++ front/que.php
@@ -1,6 +1,6 @@
 <fieldset>
     <legend>目前位置 : 首頁 > 問卷調查</legend>
-    <table style="width: 95%;">
+    <table style="width: 95%;margin:auto">
         <tr>
             <th class="ct">編號</th>
             <th class="ct">問卷題目</th>
@@ -9,16 +9,28 @@
             <th class="ct">狀態</th>
         </tr>
         <?php
-        $subjects=$Que->all('subject_id');
+        // subject_id選項
+        $subjects=$Que->all(['subject_id'=>0]);
         foreach($subjects as $key => $subject){
    
         ?>
         <tr>
-            <td><?=$key+1;?></td>
-            <td><?=$subject['text'];?></td>
-            <td></td>
-            <td></td>
-            <td></td>
+            <td class="ct"><?=$key+1;?></td>
+            <!-- $subject['text']題目 -->
+            <td class="ct"><?=$subject['text'];?></td>
+            <td class="ct"><?=$subject['count'];?></td>
+            <td class="ct">
+                <a href="?do=result&id=<?=$subject['id'];?>">結果</a>
+            </td>
+            <td class="ct">
+                <?php
+                    if(!isset($_SESSION['user'])){
+                        echo "請先登入";
+                    }else{
+                        echo "<a href='?do=vote&id={$subject['id']}'>參與投票</a>";
+                    }
+                ?>
+            </td>
         </tr>
         <?php
         }
